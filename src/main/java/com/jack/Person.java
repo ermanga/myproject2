513eda15ebc24f44b5def771876ef73c8d7a0615
Jack Su <v52222222@gmail.com>
2019-09-23 22:05:11 +0800

    Initial commit

---
+++ src/main/java/com/jack/Person.java
@@ -0,0 +1,8 @@
+package com.jack;
+
+public class Person {
+    public void hello() {
+        System.out.println("Hello world");
+    }
+
+}
---
+++ src/main/kotlin/jack/com/Hello.kt
@@ -0,0 +1,14 @@
+package jack.com
+
+fun main() {
+//    println("Hello kotlin")
+//    Person().hello()
+    val p = Person()
+    p.hello()
+}
+
+class Person {
+   fun hello() {
+       println("Hello kotlin")
+   }
+}
